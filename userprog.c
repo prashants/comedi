e515f9ba3df22ab62e87a2040ce19435e5d5714d
Prashant P Shah <pshah.mumbai@gmail.com>
2011-04-20 22:17:14 +0530

    added sample program file

---
+++ userprog.c
@@ -0,0 +1,23 @@
+#include <stdio.h>   /* for printf() */
+#include <comedilib.h>
+
+// gcc -Wall -O2 userprog.c -lcomedi -lm
+
+int subdev = 0;         /* change this to your input subdevice */
+int chan = 0;           /* change this to your channel */
+int range = 0;          /* more on this later */
+int aref = AREF_GROUND; /* more on this later */
+
+int main(int argc,char *argv[])
+{
+  comedi_t *it;
+  lsampl_t data;
+
+  it=comedi_open("/dev/comedi0");
+
+  comedi_data_read(it,subdev,chan,range,aref, & data);
+
+  printf("%d\n",data);
+
+  return 0;
+}
