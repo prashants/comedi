61a69980cfd9d99bd297f619baba53cc355cf408
Prashant Shah <pshah.mumbai@gmail.com>
2011-05-24 10:39:02 +0530

    Updated simple pci driver
    Signed-off-by: Prashant Shah <pshah.mumbai@gmail.com>

--- drivers/dyna_pci1050.c
+++ drivers/dyna_pci1050.c
@@ -16,27 +16,21 @@ unsigned long iosize0, iosize1, iosize2, iosize3, iosize4, iosize5;
 static int dyna_pci1050_read_proc(char *page, char **start, off_t
 offset, int count, int *eof, void *data)
 {
-	//unsigned short int data16;
-	u16 data16, data16_le16;
+	u16 data16;
 	unsigned int counter = 0;
 
 	printk(KERN_INFO "comedi: dyna_pci1050: %s\n", __func__);
 
 	for (counter = 0; counter <= 15; counter++) {
 		mb(); smp_mb(); mdelay(10);
-		//outw_p(0x0000 + counter, iobase2 + 2);
-		//outb_p(0x00, iobase2 + 3);
-		//outb_p(0x00 + counter, iobase2 + 2);
-		//outb_p(0x00, 0xc004);
-		outb_p(0x33 + counter, 0xc003);
+		outw_p(0x0000 + counter, iobase2 + 2);
 		mb(); smp_mb(); mdelay(10);
-
-		data16 = inw_p(0xc000);
+		data16 = inw_p(iobase2);
 		data16 &= 0x0111;
-		data16_le16 = le16_to_cpu(data16);
 		mb(); smp_mb(); mdelay(10);
-		printk(KERN_INFO "reading data iobase3 for channel %2d : %d\n", counter, data16);
+		printk(KERN_INFO "reading data iobase2 for channel %2d : %4d %04X\n", counter, data16, data16);
 	}
+
        return 0;
 }
 
@@ -64,11 +58,11 @@ pci_device_id *id)
 
        for (counter = 0; counter < 6; counter++) {
                pci_resource = pci_resource_start(dev, counter);
-               printk(KERN_INFO "comedi: dyna_pci1050 : pci_resource_start : %d : %016lX\n", counter, pci_resource);
+               printk(KERN_INFO "comedi: dyna_pci1050 : pci_resource_start : %d : %08lX\n", counter, pci_resource);
                pci_resource = pci_resource_end(dev, counter);
-               printk(KERN_INFO "comedi: dyna_pci1050 : pci_resource_end : %d : %016lX\n", counter, pci_resource);
+               printk(KERN_INFO "comedi: dyna_pci1050 : pci_resource_end : %d : %08lX\n", counter, pci_resource);
                pci_resource = pci_resource_flags(dev, counter);
-               printk(KERN_INFO "comedi: dyna_pci1050 : pci_resource_flags : %d : %016lX\n", counter, pci_resource);
+               printk(KERN_INFO "comedi: dyna_pci1050 : pci_resource_flags : %d : %08lX\n", counter, pci_resource);
        }
 
        iobase0 = pci_resource_start(dev, 0);
@@ -120,14 +114,14 @@ static struct pci_device_id ids[] = {
        { 0, },
 };
 
-MODULE_DEVICE_TABLE(pci, dyna_pci1050_driver);
-
 struct pci_driver dyna_pci1050_driver = {
        .name           = DRV_NAME,
        .probe          = dyna_pci1050_probe,
        .remove         = dyna_pci1050_remove,
        .id_table       = ids,
 };
+
+MODULE_DEVICE_TABLE(pci, dyna_pci1050_driver);
 
 static int __init dyna_pci1050_init(void)
 {
